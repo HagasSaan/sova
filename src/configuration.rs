a986b51ae5d79f7943d8ada9187f291afca8d2ce
hagassaan <artemian000@gmail.com>
2022-01-16 09:41:23 -0500

    loading config from local file, draft

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead, Error, Read};
 use serde::{Deserialize, Serialize};
 
 use crate::rule::Rule;
@@ -27,5 +29,27 @@ impl Configuration {
             behaviour_on_incidents,
             rules
         }
+    }
+
+    pub fn dump_into_yaml_file(&self, path: String) -> () {
+        let config_str = serde_yaml::to_string(&self)
+            .expect("Error on dumping config to string");
+
+        let mut output = File::create(path).expect("Error on creating file");
+
+        output.write_all(config_str.as_ref()).expect("Error on writing content to file");
+
+        ()
+    }
+
+    pub fn load_from_yaml_file(path: String) -> Self {
+        let mut input = File::open(path).expect("Error on opening file");
+        let mut config_str = String::new();
+        input.read_to_string(&mut config_str).expect("Error on reading file");
+
+        let configuration = serde_yaml::from_str(&*config_str)
+            .expect("Error on loading configuration from content");
+
+        configuration
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod analyzer_tests;
 mod record_tests;
 mod rule;
 
-use std::io;
+use std::{env, io};
+use std::env::VarError;
 use record::Record;
 use sova::Sova;
 use crate::analyzer::Analyzer;
@@ -15,23 +16,42 @@ use crate::rule::{ConditionType, Rule, Subject};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let sock_path = "/var/run/snoopy.sock".to_string();
-    let rules = vec![
-        Rule {
-            subject: Subject::CommandLine,
-            condition: ConditionType::MustBeIn,
-            objects: vec![
-                String::from("supervisorctl")
-            ],
-            behaviour_on_violation: Behaviour::KillSystem
-        }
-    ];
+    // let sock_path = "/var/run/snoopy.sock".to_string();
+    // let rules = vec![
+    //     Rule {
+    //         subject: Subject::CommandLine,
+    //         condition: ConditionType::MustBeIn,
+    //         objects: vec![
+    //             String::from("supervisorctl")
+    //         ],
+    //         behaviour_on_violation: Behaviour::KillSystem
+    //     }
+    // ];
 
-    let configuration = Configuration::new(
-        sock_path,
-        Behaviour::KillProcess,
-        rules,
-    );
+    // let configuration = Configuration::new(
+    //     sock_path,
+    //     Behaviour::KillProcess,
+    //     rules,
+    // );
+
+    // configuration.dump_into_yaml_file(String::from("/tmp/sova.yaml"));
+
+    const DEFAULT_PATH: &str = "/tmp/sova.yaml";
+
+    let path = match env::var("SOVA_CONFIG") {
+        Ok(path) => path,
+        Err(e) => {
+            println!(
+                "Config env var not found or corrupted due {:?}, using default path",
+                e.to_string(),
+            );
+            String::from(DEFAULT_PATH)
+        },
+    };
+
+    let configuration = Configuration::load_from_yaml_file(path);
+
+    println!("Configuration loaded");
 
     let sova: Sova = Sova::new(configuration);
 
